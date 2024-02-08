1db3d826de3d808e5693392dc5dd2d81346781fb
Suraj Hanchinal <surajhanchinal@gmail.com>
2024-02-08 19:16:07 +0530

    Fix a major bug in IKSolver. The reason was not using brackets in the denominator. This essentially fixed J3 to 90 degrees and did not let the model explore all the solutions. Now we should be able to play extreme shots.

--- include/ik_solver.h
+++ include/ik_solver.h
@@ -77,20 +77,33 @@ class IKSolver
     float c2_e = c2(j5e, j234e, Pz);
     float k1_e = k1();
     float k2_e = k2();
-    return acos((c1_e * c1_e + c2_e * c2_e - k1_e * k1_e - k2_e * k2_e) / 2 * k1_e * k2_e);
+    float num = c1_e * c1_e + c2_e * c2_e - k1_e * k1_e - k2_e * k2_e;
+    float den = 2 * k1_e * k2_e;
+    return acos(num/den);
   }
 
-  std::pair<float, float> j2(float j3e, float j1e, float j5e, float j234e, float Px, float Py, float Pz)
+  std::vector<float> j2(float j3e, float j1e, float j5e, float j234e, float Px, float Py, float Pz)
   {
     float c1_e = c1(j1e, j5e, j234e, Px, Py);
     float c2_e = c2(j5e, j234e, Pz);
     float k1_e = k1();
     float k2_e = k2();
     float t1 = atan(c1_e / c2_e);
+    float t11 = t1 + M_PI_2;
+    float t12 = t1 - M_PI_2;
+    std::vector<float> t1_e = {t1,t11,t12};
     float t2 = atan(k2_e * sin(j3e) / (k1_e + k2_e * cos(j3e)));
-    float sol1 = t1 - t2;
-    float sol2 = t1 + t2;
-    return { sol1, sol2 };
+    float t21 = t2 + M_PI_2;
+    float t22 = t2 - M_PI_2;
+    std::vector<float> t2_e = {t2,t21,t22};
+    std::vector<float> sols;
+    for(int i=0;i<3;i++){
+      for(int j=0;j<3;j++){
+        sols.push_back(t1_e[i] - t2_e[j]);
+        sols.push_back(t1_e[i] + t2_e[j]);
+      }
+    }
+    return sols;
   }
 
   float j1(float tyy, float txy)
@@ -221,12 +234,12 @@ class IKSolver
     
     float j3e = j3(j1e,j5e,j234e,Px,Py,Pz);
     auto j2e = j2(j3e,j1e,j5e,j234e,Px,Py,Pz);
-    float j4_e1 = j234e - j3e - j2e.first;
-    float j4_e2 = j234e + j3e - j2e.second;
-    //sols.push_back({.j1=j1e,.j2=j2e.first,.j3=j3e,.j4=j4_e1,.j5=j5e});
-    //sols.push_back({.j1=j1e,.j2=j2e.second,.j3=-j3e,.j4=j4_e2,.j5=j5e});
-    sols.push_back({.j1=getOptimalAngle(j1e),.j2=getOptimalAngle(j2e.first),.j3=getOptimalAngle(j3e),.j4=getOptimalAngle(j4_e1),.j5=getOptimalAngle(j5e),.t = t});
-    sols.push_back({.j1=getOptimalAngle(j1e),.j2=getOptimalAngle(j2e.second),.j3=getOptimalAngle(-j3e),.j4=getOptimalAngle(j4_e2),.j5=getOptimalAngle(j5e),.t = t});
+    for(float & j2 : j2e){
+      float j4_e1 = j234e - j3e - j2;
+      float j4_e2 = j234e + j3e - j2;
+      sols.push_back({.j1=getOptimalAngle(j1e),.j2=getOptimalAngle(j2),.j3=getOptimalAngle(j3e),.j4=getOptimalAngle(j4_e1),.j5=getOptimalAngle(j5e),.t = t});
+      sols.push_back({.j1=getOptimalAngle(j1e),.j2=getOptimalAngle(j2),.j3=getOptimalAngle(-j3e),.j4=getOptimalAngle(j4_e2),.j5=getOptimalAngle(j5e),.t = t});
+    }
   }
 
   void full_ik(Matrix4f &pose_matrix){
@@ -255,7 +268,7 @@ class IKSolver
   void position_ik(float Px,float Py,float Pz,double t, std::vector<Solution> &sols){
     std::vector<Solution> all_sols;
     for(int j1d=-90;j1d<=90;j1d+=10){
-        for(int j234d=-179;j234d<=179;j234d+=10){
+        for(int j234d=-91;j234d<=91;j234d+=10){
             float j1e = j1d*(M_PI/180);
             float j234e = j234d*(M_PI/180);
             float j5e = j5_new(j1e,Px,Py);
